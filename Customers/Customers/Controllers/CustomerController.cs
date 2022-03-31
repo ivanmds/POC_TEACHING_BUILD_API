f50684788212241b1509455c1a8535d87723e04c
Ivan Meireles da Silva <ivan.silva@tooseguros.com.br>
2022-03-31 20:08:50 -0300

    trocando post para get

--- Customers/Customers/Controllers/CustomerController.cs
+++ Customers/Customers/Controllers/CustomerController.cs
@@ -10,7 +10,7 @@ namespace Customers.Controllers
         // post => criar recurso do lado do servido
         
         
-        [HttpPost]
+        [HttpGet]
         public  IActionResult Post()
         {
 
